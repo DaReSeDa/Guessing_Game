1842ee0e15cad75f9122f8483c928ac03a51c758
DaReSeDa <148769155+DaReSeDa@users.noreply.github.com>
2023-11-29 21:27:46 +0100

    Update randomguess.py

--- randomguess.py
+++ randomguess.py
@@ -1,58 +1,88 @@
+
 import random
 
-n = random.randint(1, 100)
-counter = 1
-print(f"THIS IS A GUESSING GAME------OF 1 - 100------- YOU ONLY HAVE 5 TRIES")
-while True:
 
-    try:
-
-        user_input = int(input(f'This is the {counter} Guess \n>  '))
-        break
-    except ValueError as er:
-        print('Sorry the number must be integer')
+def get_random():
+    number_generate = random.randint(1, 100)
+    return number_generate
 
 
-while counter <= 4:
+def play_game():
+    game_counter = 1
+    game_chances = 5
 
-    if user_input == n and counter == 1:
-        print('Amazing you got the number on your first try')
-        dy = """
+    n = get_random()
 
-                    oooooooooo       oooooo
-                    oo             oo      oo  
-                    oo   ooooo     oo      oo
-                    oo      oo     oo      oo
-                    oooooooooo       oooooo
+    print(f"THIS IS A GUESSING GAME------OF 1 - 100------- YOU ONLY HAVE 5 TRIES")
 
-                    """
-        print(dy)
-        exit()
+    while True:
 
-    if user_input > 100:
-        print("THIS IS A GUESSING GAME------OF 1 - 100\n>  ")
-        counter -= 1
+        try:
+            print()
+            print("*" * 30)
+            print(f"Guess {game_counter} of {game_chances}")
+            print("*" * 30)
+            user_input = int(input('>  '))
+            break
+        except ValueError as er:
+            print('Sorry the number must be integer')
 
-    counter += 1
+    while game_counter < game_chances:
 
-    if user_input == n:
-        print(f'You have guessed correctly\tThe number is {n}')
-        exit()
-    else:
-        print(f"{user_input} This number doesn't match")
+        if user_input == n and game_counter == 1:
+            print('Amazing you got the number on your first try')
+            dy = """
+    
+                        oooooooooo       oooooo
+                        oo             oo      oo  
+                        oo   ooooo     oo      oo
+                        oo      oo     oo      oo
+                        oooooooooo       oooooo
+    
+                        """
+            print(dy)
+            break
 
-    if user_input < n:
-        print(f'The number {user_input} is less than the real number')
-        print('TRY AGAIN!!!\n')
+        if user_input > 100:
+            print("THIS IS A GUESSING GAME------OF 1 - 100\n>  ")
+            game_counter -= 1
 
-    elif user_input > n:
-        print(f'The number {user_input} is greater than the real number')
-        print('TRY AGAIN!!!\n')
-    user_input = int(input(f'This is the {counter} Guess\n>  '))
+        game_counter += 1
 
-    if user_input == n:
-        print(f'You have guessed correctly\tThe number is {n}')
-        exit()
+        if user_input == n:
+            print(f'You have guessed correctly\tThe number is {n}')
+            break
+        else:
+            print(f"{user_input} This number doesn't match")
 
-print('SORRY 0_0 you ran out of guess numbers')
-print(f'The right number is {n}')
+        if user_input < n:
+            print(f'The number {user_input} is less than the real number')
+            print('TRY AGAIN!!!\n')
+
+        elif user_input > n:
+            print(f'The number {user_input} is greater than the real number')
+            print('TRY AGAIN!!!\n')
+
+        while True:
+
+            try:
+                print()
+                print("*" * 30)
+                print(f"Guess {game_counter} of {game_chances}")
+                print("*" * 30)
+
+                user_input = int(input('>  '))
+                break
+            except ValueError as er:
+                print('Sorry the number must be integer')
+
+        if user_input == n:
+            print(f'You have guessed correctly\tThe number is {n}')
+            break
+
+    print('SORRY 0_0 you ran out of guess numbers')
+    print(f'The right number is {get_random()}')
+
+
+if __name__ == '__main__':
+    play_game()
